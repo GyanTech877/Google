da946fd286b22f80806e58b9fc11625ef35a85d7
Tech Gyan <53473704+GyanTech877@users.noreply.github.com>
2019-12-26 17:12:25 +0530

    Update MergeIntervals.java

--- Arrays/MergeIntervals.java
+++ Arrays/MergeIntervals.java
@@ -16,3 +16,39 @@ This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
 Make sure the returned intervals are also sorted.
 */
 
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+
+public class Solution {
+    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+         ArrayList<Interval> result = new ArrayList<>();
+         if(intervals == null || intervals.size()==0){
+             result.add(newInterval);
+             return result;
+         }
+         else if(newInterval ==null) return intervals;
+         int i = 0;
+         while(i<intervals.size() && intervals.get(i).end<newInterval.start){
+                result.add(intervals.get(i++));
+         }
+         while(i<intervals.size() && intervals.get(i).start<=newInterval.end) {
+                newInterval = new Interval(
+                Math.min(intervals.get(i).start,newInterval.start),  
+                Math.max(intervals.get(i).end,newInterval.end)
+                ); 
+                i++;
+         }
+         result.add(newInterval);
+         while(i<intervals.size()) {
+            result.add(intervals.get(i++)); 
+         }
+         return result;
+    }
+}
